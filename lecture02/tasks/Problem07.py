aea281ff9b914570249a78e32bba766a0e11c1a1
Chavdar Momchilov <chavmom@gmail.com>
2024-06-17 09:29:45 +0300

    Changes to be committed: 	new file:   lecture02/tasks/Problem07.py

---
+++ lecture02/tasks/Problem07.py
@@ -0,0 +1,7 @@
+# Area of rectangle = length * width
+length = float(input("Enter length = "))
+width = float(input("Enter width = "))
+area = length * width
+print(f"Area of rectangle = {area}")
+perametar = 2 * (length + width)
+print(f'Perametar = {perametar}')
