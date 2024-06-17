6a1777f30102998883802c98fc92f92c00b6e4d4
Chavdar Momchilov <chavmom@gmail.com>
2024-06-17 09:48:58 +0300

    Changes to be committed: 	new file:   lecture02/tasks/Problem09.py

---
+++ lecture02/tasks/Problem09.py
@@ -0,0 +1,10 @@
+import math
+
+# area of circle = math.pi * r ** 2
+
+r = float(input("Enter circle r = "))
+area = math.pi * (r ** 2)
+
+print(f'pi = {math.pi}')
+print(f'Area of circle = {area}')
+
