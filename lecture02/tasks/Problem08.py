ed9f350616f5d499651cca7cc5d507d5f87641c5
Chavdar Momchilov <chavmom@gmail.com>
2024-06-17 09:36:42 +0300

    Changes to be committed: 	new file:   lecture02/tasks/Problem08.py

---
+++ lecture02/tasks/Problem08.py
@@ -0,0 +1,10 @@
+import math
+
+# Pitagor theorem a ** 2 + b ** 2 = c ** 2
+# a,b, input
+
+a = int(input("Enter a = "))
+b = int(input("Enter b = "))
+c = math.sqrt((a ** 2 + b ** 2))
+print(f"c = {c}")
+
