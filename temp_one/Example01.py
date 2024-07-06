203d6d8f103a058829716860d13cc6b9b493d873
chocho77 <chavmom@gmail.com>
2024-07-06 18:39:27 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   temp_one/Example01.py

---
+++ temp_one/Example01.py
@@ -0,0 +1,12 @@
+# tutorial sites 
+# https://www.programiz.com/python-programming/args-and-kwargs
+# https://realpython.com/python-kwargs-and-args/
+
+# Example 1: Function to add 3 numbers
+
+#def adder(x,y,z):
+#    print("sum:",x+y+z)
+
+#adder(10,12,13)
+
+# continue in home 
